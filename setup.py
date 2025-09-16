49b5500dbda66f2787648bf2afb802ec734aa9ff
Olivier998 <olivier.lefebvre3@usherbrooke.ca>
2025-09-16 16:10:14 -0400

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="MED3pa",
-    version="1.0.0b0",
+    version="1.0.0b1",
     author="MEDomics consortium",
     author_email="medomics.info@gmail.com",
     description="Python Open-source package for ensuring robust and reliable ML models deployments",
